5ca8ff4f62ad4a97932d56e4eb7f5a6c92b17582
leekeifuture <opnoe09@gmail.com>
2020-06-15 09:32:11 +0300

    Create initial User DTO

---
+++ src/main/java/com/company/timetable/dto/User.java
@@ -0,0 +1,16 @@
+package com.company.timetable.dto;
+
+public class User {
+
+    private Long id;
+    private Long telegramChatId;
+    private Long countryId;
+    private Long cityId;
+    private Long eduTypeId;
+    private Long eduId;
+    private Long schoolClassId;
+    private Long classLetterId;
+    private Long facultyId;
+    private Long groupId;
+    private Long adminRoleId;
+}
